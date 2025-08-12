588ca8a4ce405cf071d3c7653d9b38878d5dfb49
Laiane Sores <161250700+SoaresLai@users.noreply.github.com>
2025-08-12 12:06:17 -0300

    Delete CoberturasCasadosToldos/Front directory

--- CoberturasCasadosToldos/Front/src/components/ui/collapsible.tsx
+++
@@ -1,9 +0,0 @@
-mport * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
-
-const Collapsible = CollapsiblePrimitive.Root
-
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
-
-export { Collapsible, CollapsibleTrigger, CollapsibleContent }
